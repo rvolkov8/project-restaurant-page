e6870dc6dafb56182004df5e587f0d66ee7cfc34
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-03-22 00:01:41 +0200

    Create a homepage for the restaurant
    - Created homepage structure using JS and styled it (both in separate files home-page).
    - Installed style and CSS loaders for webpack.
    - Imported the JS and CSS code to the index.js file and called it.

---
+++ src/home-page.js
@@ -0,0 +1,36 @@
+export default function createHomePage() {
+  const contentDiv = document.querySelector('#content');
+
+  const topMenuDiv = document.createElement('div');
+  topMenuDiv.classList.add('top-menu');
+
+  const homeCategory = document.createElement('div');
+  homeCategory.textContent = 'Home';
+
+  const menuCategory = document.createElement('div');
+  menuCategory.textContent = 'Menu';
+
+  const contactsCategory = document.createElement('div');
+  contactsCategory.textContent = 'Contacts';
+
+  topMenuDiv.appendChild(homeCategory);
+  topMenuDiv.appendChild(menuCategory);
+  topMenuDiv.appendChild(contactsCategory);
+
+  const mainInfoDiv = document.createElement('div');
+  mainInfoDiv.classList.add('main-info');
+
+  const headline = document.createElement('div');
+  headline.classList.add('headline');
+  headline.textContent = 'MICHI RAMEN';
+
+  const subheadline = document.createElement('div');
+  subheadline.classList.add('subheadline');
+  subheadline.textContent = 'Online ordering is available';
+
+  mainInfoDiv.appendChild(headline);
+  mainInfoDiv.appendChild(subheadline);
+
+  contentDiv.appendChild(topMenuDiv);
+  contentDiv.appendChild(mainInfoDiv);
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './home-page.css';
+import createHomePage from './home-page';
+
+createHomePage();
